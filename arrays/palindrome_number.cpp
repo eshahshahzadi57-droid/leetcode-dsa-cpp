b63edcfce3365f620daac93f2b72638e0d4f6abc
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2025-12-31 19:41:47 +0500

    Add Palindrome Number Solution

---
+++ arrays/palindrome_number.cpp
@@ -0,0 +1,22 @@
+// LeetCode #9 - Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int n=x,temp=x;
+        long long reverse =0;
+        while(n!=0){
+            int r=n%10;
+            reverse=reverse*10+r;
+            n=n/10;
+        }
+        return temp==reverse;
+    }
+};
