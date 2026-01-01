dd32af36f2d63a37f63d03c2fa9fd4ef49eead75
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2026-01-01 19:52:56 +0500

    Update the file

---
+++ arrays/14_common_prefix.cpp
@@ -0,0 +1,23 @@
+// LeetCode # 14- Longest Common prefix
+// https://leetcode.com/problems/longest-common-prefix/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    string longestCommonPrefix(vector<string>& strs){
+        string prefix=strs[0];
+        for (int i = 1; i < strs.size(); i++)
+        {
+            while(strs[i].find(prefix)!=0){
+                prefix=prefix.substr(0, prefix.size()-1);
+                if(prefix.empty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+};
