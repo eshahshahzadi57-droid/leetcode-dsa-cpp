c31633a109bab32998b00ce61e8f2061d5898635
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2026-01-06 00:40:14 +0500

    Add new file

---
+++ arrays/26_remove_duplicate.cpp
@@ -0,0 +1,17 @@
+// LeetCode # 26- Remove duplicate from sorted array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    int removeDuplicates(vector<int>& nums){
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            
+        }
+        
+    }
+};
