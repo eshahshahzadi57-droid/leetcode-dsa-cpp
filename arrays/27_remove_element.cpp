fc39dc9f9169abb2259f4396c2be97f2ab50c309
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2026-01-06 13:22:48 +0500

    Update content

--- arrays/27_remove_element.cpp
+++ arrays/27_remove_element.cpp
@@ -1,10 +1,21 @@
 // LeetCode # 27- Remove element from array
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution{
 public:
-    
+    int removeElement(vector<int>& nums, int val){
+        int k=0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
 
 };
