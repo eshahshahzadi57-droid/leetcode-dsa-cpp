f3d56cd1649fd7b781cbc39fa6dcf8b3f35a2381
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2026-01-07 21:19:43 +0500

    Update content

--- arrays/35_search_insert_pos.cpp
+++ arrays/35_search_insert_pos.cpp
@@ -1,5 +1,10 @@
 // LeetCode # 35- Search Insert Position
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+class Solution{
+public:
+    int searchInsert(vector<int>& nums, int target);
+};
