04dcb26d0f93d9c03044aa49141ad16113af0260
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2026-06-18 23:51:53 +0500

    Solver Prob # 189

---
+++ arrays/189_rotate_array.cpp
@@ -0,0 +1,33 @@
+// LeetCode # 189 - Rotate Array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n==0) return;
+        k %=n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k , n-1);
+    }
+
+private:
+void reverse(vector<int>& nums, int left, int right){
+        while(left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+};
+
+int main(){
+    
+    return 0;
+}
