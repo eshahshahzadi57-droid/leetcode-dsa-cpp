9272aeace0e1f6357b81374d3055ad281d709deb
Eshah Shahzadi <eshahshahzadi57@gmail.com>
2026-06-25 23:27:46 +0500

    Solved Prob 121

---
+++ arrays/121_buy_and_sell_stock.cpp
@@ -0,0 +1,17 @@
+// Leetcode # 121 - Best time to buy and sell stock 
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for(int price : prices){
+            minPrice=min(minPrice, price);
+
+            maxProfit=max(maxProfit, price-minPrice);
+        }
+        return maxProfit;
+    }
+};
